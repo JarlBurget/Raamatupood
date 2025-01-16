656f038d949995ed2dfd0cba9bd23af35492a5f2
Jarl <jarl-ivor.burget@voco.ee>
2025-01-16 18:37:47 +0200

    2 ülesanne poolik

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
+const db = require('../utils/db'); // Lisa andmebaasiühendus
 
-router.get('/', (req, res, next) =>{
-    res.send('<b>Web shop page</b>');
+router.get('/', (req, res, next) => {
+    // Tõmba andmed products tabelist
+    db.execute('SELECT * FROM products')
+        .then(([rows]) => {
+            // Renderda EJS mall toodete kuvamiseks
+            res.render('shop', { 
+                products: rows, 
+                pageTitle: 'Web Shop', 
+                path: '/' 
+            });
+        })
+        .catch(err => console.log(err));
 });
 
 module.exports = router;
